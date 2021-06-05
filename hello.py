ddd827ddad543326dbca6db2e223f6bd8491f91c
DevGM1 <84422216+DevGM1@users.noreply.github.com>
2021-06-04 22:11:14 -0400

    Update hello.py

--- hello.py
+++ hello.py
@@ -3,3 +3,7 @@ print(message)
 print(" ")
 print ("This is just me testing out my new knowledge about python.")
 print ("I'm actually going to be working on a game soon.")
+print(" ")
+print(" ")
+update = 'Update! Planning for the game has started.'
+print(update)
