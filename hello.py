7c5506de30560b2e13280824b846f7d87ebe058e
BotanicProgrammer <84422216+BotanicProgrammer@users.noreply.github.com>
2022-03-13 21:17:02 -0400

    Update hello.py

--- hello.py
+++ hello.py
@@ -2,14 +2,6 @@ message = 'Hello'
 print(message)
 print(" ")
 print ("This is just me testing out my new knowledge about python.")
-print ("I'm actually going to be working on a game soon.")
-print(" ")
-print(" ")
-update = 'Update! Planning for the game has started.'
-print(update)
-update2 = 'Sadly, because of copyright issues, we cannot continue progress with the game.'
-print(update2)
-
-print("that was then, this is now. I have original ideas now. I am just not good enough at programming to use those ideas yes though.")
+print ("I'm actually also learning Javascript and C++")
 
 # July 20 2021
