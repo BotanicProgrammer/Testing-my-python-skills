7a49d0bce9d172cb6dc40c9b01ae9568fb5573af
BotanicProgrammer <84422216+BotanicProgrammer@users.noreply.github.com>
2022-08-29 22:11:26 -0400

    Update hello.py

--- hello.py
+++ hello.py
@@ -3,5 +3,10 @@ print(message)
 print(" ")
 print ("This is just me testing out my new knowledge about python.")
 print ("I'm actually also learning Javascript and C++")
-
 # July 20 2021
+print(" ")
+
+# August 29 2022
+years_learning_python = 1
+print years_learning_python
+
